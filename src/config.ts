e77804d6c3b3a4d8d323f243686a312968e13dcb
Gaurav Gorade <gauravgorade2929@gmail.com>
2025-07-10 08:54:26 +0530

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const config = {
   PORT: 4000,
-  DATABASE_URL: "postgresql://analytics_user:analytics_pass@172.20.235.173:5432/analytics",
-  JWT_SECRET: "PutlerAnalytics0987$@"
+  // DATABASE_URL: "postgresql://analytics_user:analytics_pass@172.20.235.173:5432/analytics",
+  DATABASE_URL:"postgresql://postgres:fFlLqshONpcWVOUGSutqmyhNhZAeraMK@maglev.proxy.rlwy.net:43913/railway",
+  JWT_SECRET: "PutlerAnalytics0987$@",
 };
